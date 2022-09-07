fb9291b11e058c2a95185c675ddcf67af670d32d
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-09-06 22:13:14 -0400

    Updated comments

--- Open Multiple Views/Open Multi Views_script.py
+++ Open Multiple Views/Open Multi Views_script.py
@@ -1,47 +1,16 @@
-''' 
-Opens multiple selected views & sheets, then tiles active windows. 
-''' 
+# Opens multiple selected views & sheets in a batch 
 
-# read current user selection 
-# for v in views (including both views & sheets), activate view 
-# tile all activated views 
+import rpw 
+from rpw import ui, db, doc 
+from pyrevit import revit, DB, UI 
+from pyrevit import forms 
+from Autodesk.Revit.UI import UIApplication, RevitCommandId, PostableCommand 
 
-importrpw 
-fromrpwimportui, db, doc 
-frompyrevitimportrevit, DB, UI 
-frompyrevitimportforms 
-fromAutodesk.Revit.UIimportUIApplication, RevitCommandId, PostableCommand 
+# Read current user selection (views and sheets from Revit Project Browser)
+selection = revit.get_selection() 
 
-selection=revit.get_selection() 
-
-forvinselection:
-    v_to_open=revit.doc.GetElement(v.Id) 
-	revit.uidoc.ActiveView=v_to_open 
-
-# activating views does NOT work in a transaction, but works fine kept OUTSIDE a transaction 
-
-
-''' 
-uiapp = UIApplication(doc.Application) ​ 
-
-tile_windows = UI.RevitCommandId.LookupPostableCommandId(UI.PostableCommand.TileWindows) 
-uiapp.PostCommand(tile_windows) 
-''' 
-
-​''' 
-zoom_extents = UI.RevitCommandId.LookupPostableCommandId(UI.PostableCommand.TileWindows) 
-#uiapp = UIApplication(doc.Application) 
-uiapp.PostCommand(zoom_extents) 
-''' 
-
-''' 
-with rpw.db.Transaction('Multi Open Views & Sheets'): 
-    try: 
-        for v in selection: 
-            v_to_open = revit.doc.GetElement(v.Id) 
-            revit.uidoc.ActiveView = v_to_open 
-        else: 
-            pass 
-    except: 
-        pass 
-''' 
+# Loop through selection and activate each view
+# Note:  view activation does NOT work within a Revit transaction, but works fine outside of a transaction
+for v in selection:
+    v_to_open = revit.doc.GetElement(v.Id) 
+	revit.uidoc.ActiveView = v_to_open
